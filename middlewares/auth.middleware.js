294fbcde339e1a3e92b2d35e386a983840c95f8c
hugotata78 <hugotatarinoff@gmail.com>
2022-09-27 15:44:17 -0300

    correcciones middleware de autenticación

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,25 +3,28 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 module.exports = {
-    async Auth(req, res, next) {
+   Auth(req, res, next) {
         try {
 
             const strToken = req.headers.authorization
             if (!strToken) return res.status(403).json({ msg: 'You must login!' })
             const token = strToken.split(' ').length == 2 ? strToken.split(' ')[1] : strToken
-            const decoded = jwt.verify(token, process.env.PRIVATE_KEY)
-            const user = await User.findOne({
-                where: {
-                    id: decoded.id
-                },
-                include: {
-                    model: Role,
-                    as: 'roles'
-                }
+            jwt.verify(token, process.env.PRIVATE_KEY, async (err,decoded)=>{
+                if(err) return res.status(500).json({err})
+                const user = await User.findOne({
+                    where: {
+                        id: decoded.id
+                    },
+                    include: {
+                        model: Role,
+                        as: 'roles'
+                    }
+                })
+                if (!user) return res.status(401).json({ msg: 'Access denied!' })
+                req.user = user
+                next()
             })
-            if (!user) return res.status(401).json({ msg: 'Access denied!' })
-            req.user = user
-            next()
+            
         } catch (error) {
             res.status(500).json({ error })
         }
