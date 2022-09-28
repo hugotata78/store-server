9c5791ede855f9ec5beccc21e375eb0c9d56173b
hugotata78 <hugotatarinoff@gmail.com>
2022-09-28 10:26:16 -0300

    mejoras en validaciones para registro de categorias

--- validators/category.validators.js
+++ validators/category.validators.js
@@ -4,18 +4,21 @@ const { Category } = require('../models')
 
 const categoryValidator = [
     check('name')
-        .not()
-        .isEmpty()
-        .withMessage('The name field is required!')
-        .isLength({ min: 3 })
-        .withMessage('Name must contain at least three letters')
         .custom(async (value) => {
-            const category = await Category.findOne({
-                where: {
-                    name: value
+            if(!value){
+                throw new Error('The name field is required!');
+            }else{
+                if(value.length < 3){
+                    throw new Error('Name must contain at least three letters!');
+                }else{
+                    const category = await Category.findOne({
+                        where: {
+                            name: value
+                        }
+                    })
+                    if (category) throw new Error('The name entered is already in use!');
                 }
-            })
-            if (category) throw new Error('The name entered is already in use!');
+            }
         }),
     check('description')
         .not()
