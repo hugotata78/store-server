adf0feb6961cddf63c9ba6469b5b28b946c600ae
hugotata78 <hugotatarinoff@gmail.com>
2022-09-29 21:34:29 -0300

    config app

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const app = express()
 app.set('Server','Servidor')
 app.set('port',process.env.PORT || 4000)
 
-app.use(cors())
+app.use(cors({
+    origin:`${process.env.HOST}:${app.get('port')}`
+}))
 //app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
